b8384915d400f83f18d09e16b7ff6983ea3b9798
Massimiliano Poletto <max.poletto@gmail.com>
2021-01-13 23:05:44 +0100

    more condensed font, black on white

--- logo/genlogo.py
+++ logo/genlogo.py
@@ -11,22 +11,23 @@ svg = """<svg version="1.1" xmlns="http://www.w3.org/2000/svg"
      viewBox="0, 0, {sz}, {sz}">
   <defs>
     <style>
-       @import url('https://fonts.googleapis.com/css2?family=Barlow+Semi+Condensed&display=swap');
+       @import url('https://fonts.googleapis.com/css2?family=Barlow+Condensed&display=swap');
     </style>
   </defs>
   <g id="g1">
-    <path d="M0,0 L{sz},0 L{sz},{sz} L0,{sz} L0,0 z" fill="#000000"/>
+    <path d="M0,0 L{sz},0 L{sz},{sz} L0,{sz} L0,0 z" fill="#ffffff" stroke="{bc}" stroke-width="32"/>
     <path d="M{sl},{sb} L{sm},{st} L{sr},{sb} z" fill="{sc}"/>
     <path d="M{ml},{mb} L{mm},{mt} L{mr},{mb} z" fill="{mc}"/>
     <path d="M{bl},{bb} L{bm},{bt} L{br},{bb} z" fill="{bc}"/>
-    <text x="{tx}" y="{ty}">
-      <tspan dx="{dx}" font-family="Barlow Semi Condensed" font-size="{fs}" fill="#FFFFFF">
+    <text x="50%" y="95%" dominant-baseline="auto" text-anchor="middle">
+      <tspan dx="{dx}" font-family="Barlow Condensed" font-size="{fs}" fill="#000000">
         LA FORESTA
       </tspan>
     </text>
   </g>
 </svg>"""
 
+#cd9832
 # Necessary for font import.
 html = len(sys.argv) == 2 and sys.argv[1].endswith('-html')
 
@@ -36,8 +37,8 @@ class Tree:
         self.b, self.t = round(y), round(y-h)
         self.c = c
 
-fontsz = sz*41/216
-margin_frac = 1/16
+fontsz = sz*49/216
+margin_frac = 8/128
 margin_top = sz * margin_frac
 margin_left = sz * margin_frac
 epsilon = margin_top/8
@@ -57,7 +58,7 @@ print(svg.format
        sl=s.l,sm=s.m,sr=s.r,sb=s.b,st=s.t,sc=s.c,
        ml=m.l,mm=m.m,mr=m.r,mb=m.b,mt=m.t,mc=m.c,
        bl=b.l,bm=b.m,br=b.r,bb=b.b,bt=b.t,bc=b.c,
-       fs=int(fontsz), dx=-int(fontsz/11),
+       fs=int(fontsz), dx=-int(fontsz/30),
        tx=round(margin_left),ty=round(margin_top+h_orig+fontsz),
        ))
 if html:
