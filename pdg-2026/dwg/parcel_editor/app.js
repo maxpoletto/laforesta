43bafbb8685c153460482b79d272ddc0401ebb6a
Massimiliano Poletto <max.poletto@gmail.com>
2026-01-25 16:19:33 +0100

    Initial parcels geojson file

--- pdg-2026/dwg/parcel_editor/app.js
+++ pdg-2026/dwg/parcel_editor/app.js
@@ -593,4 +593,4 @@ const ParcelEditor = (function() {
     };
 })();
 
-document.addEventListener('DOMContentLoaded', () => ParcelEditor.init("all_parcels.json"));
+document.addEventListener('DOMContentLoaded', () => ParcelEditor.init("parcels.json"));
